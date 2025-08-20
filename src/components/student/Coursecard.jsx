09716a1b63810bad33836008b210152cca71f77f
Harsh06045 <harsh.kumar60456@gmail.com>
2025-08-20 12:01:29 +0530

    ..

--- src/components/student/Coursecard.jsx
+++ src/components/student/Coursecard.jsx
@@ -3,12 +3,23 @@ import { AppContext } from '../../context/AppContext';
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
 
+// Predefined prices you want to show
+const predefinedPrices = [9200, 5500, 7500, 6400, 6500, 5400];
+
 const Coursecard = ({ course }) => {
   const { currency, calculateRating } = useContext(AppContext);
 
-  // Generate random price between 5000 and 10000, rounded to nearest 100
-  const rawPrice = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000;
-  const price = Math.round(rawPrice / 100) * 100;
+  // Generate a consistent index based on course._id string
+  const getPriceIndex = (id) => {
+    let hash = 0;
+    for (let i = 0; i < id.length; i++) {
+      hash = id.charCodeAt(i) + ((hash << 5) - hash);
+      hash = hash & hash; // Convert to 32bit integer
+    }
+    return Math.abs(hash) % predefinedPrices.length;
+  };
+
+  const price = predefinedPrices[getPriceIndex(course._id)];
 
   return (
     <Link to={'/course/' + course._id} onClick={() => scrollTo(0, 0)} className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg'>
