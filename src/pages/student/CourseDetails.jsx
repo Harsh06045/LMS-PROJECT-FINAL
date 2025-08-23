abe8640aea45cd2452694a3d371dbf9abaabf48f
Harsh06045 <harsh.kumar60456@gmail.com>
2025-08-23 21:12:39 +0530

    ..

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -9,7 +9,7 @@ const CourseDetails = () => {
   const { allCourses } = useContext(AppContext)
 
   const fetchCourseData = () => {
-    if (!allCourses || allCourses.length === 0) return;
+    if (!allCourses || allCourses.length === 0) return
     const findCourse = allCourses.find(course => course._id === id)
     setCourseData(findCourse)
   }
@@ -19,15 +19,16 @@ const CourseDetails = () => {
   }, [id, allCourses])
 
   return courseData ? (
-    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 
-      px-8 md:pt-30 pt-20 text-left'>
+    <div className="flex flex-col md:flex-row gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left transition-colors duration-300
+      bg-white dark:bg-[#181e29] text-gray-900 dark:text-gray-100 min-h-screen">
       
-      <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
+      <div className="absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70 dark:from-cyan-600/50"></div>
 
-      <div className='max-w-xl x-10 text-gray-500'>
-        <h1 className='md:text-course-details-heading-large text-course-details-heading-small
-        font-semibold text-gray-800'>{courseData.courseTitle}</h1>
-        <p>
+      <div className="max-w-xl x-10">
+        <h1 className="md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800 dark:text-white">
+          {courseData.courseTitle}
+        </h1>
+        <p className="pt-4 md:text-base text-sm text-gray-700 dark:text-gray-300">
           {courseData.courseDescription
             .replace(/<[^>]*>/g, '')  // remove HTML tags
             .slice(0, 188)}
