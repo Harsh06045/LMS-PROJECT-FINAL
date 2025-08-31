2560376b7dd7802fa41eae62fbbeea1ddfcac6b0
Harsh06045 <harsh.kumar60456@gmail.com>
2025-08-31 09:49:25 +0530

    ..

--- Server/controllers/webhooks.js
+++ Server/controllers/webhooks.js
@@ -13,14 +13,14 @@ export const clerkWebhooks = async (req, res) => {
       "svix-signature": req.headers["svix-signature"],
     });
 
-    const { data, type } = evt;
+    const { data, type } = req.body;
 
     switch (type) {
       case "user.created": {
         const userData = {
           _id: data.id,
           email: data.email_addresses[0].email_address,
-          name: `${data.first_name} ${data.last_name}`,
+          name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
         await User.create(userData);
@@ -31,7 +31,7 @@ export const clerkWebhooks = async (req, res) => {
       case "user.updated": {
         const userData = {
           email: data.email_addresses[0].email_address,
-          name: `${data.first_name} ${data.last_name}`,
+          name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
         await User.findByIdAndUpdate(data.id, userData);
@@ -46,7 +46,7 @@ export const clerkWebhooks = async (req, res) => {
       }
 
       default:
-        res.status(200).json({});
+        // res.status(200).json({});
         break;
     }
   } catch (error) {
