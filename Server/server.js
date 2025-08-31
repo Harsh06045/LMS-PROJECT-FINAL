7320e7da2136447a642f012d78b835a2a2c13ba9
abhinav-dude <abhinavsrihari123@gmail.com>
2025-08-31 16:31:24 +0530

    ..

--- Server/server.js
+++ Server/server.js
@@ -14,12 +14,12 @@ await connectDB()
 app.use((cors))
 
 //Routes
-app.get('/', (req, res)=> res.send("API WORKING"))
+app.get('/', (req, res) => res.send("API WORKING"))
 app.post('/clerk', express.json(), clerkWebhooks)
 
 //Port
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
+app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
 })
