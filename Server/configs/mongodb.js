bb107094ad8b7617a7de6201d79a1847a277f783
Harsh06045 <harsh.kumar60456@gmail.com>
2025-09-01 20:44:43 +0530

    ..

--- Server/configs/mongodb.js
+++ Server/configs/mongodb.js
@@ -1,17 +1,10 @@
 import mongoose from "mongoose";
 
-// Add event listeners once
-mongoose.connection.on('connected', () => console.log('Database Connected'));
-mongoose.connection.on('error', (err) => console.error('MongoDB connection error:', err));
-
 // Connect to the MongoDB database
-const connectDB = async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/edulearn`);
-    } catch (error) {
-        console.error('Failed to connect to MongoDB:', error.message);
-        process.exit(1); // Optional: exit process on failure
-    }
-};
 
-export default connectDB;
+const connectDB = async ()=>{
+    mongoose.connection.on('connected', ()=> console.log('Database Connected'))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/edulearn`)
+}
+export default connectDB
